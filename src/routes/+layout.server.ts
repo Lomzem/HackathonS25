bccad120256dfe9baa65b9e65918ee75ca73669c
riley-1995 <rileydenn@gmail.com>
2025-04-25 18:33:51 -0700

    Update documentation for +layout.svelte and +layout.server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -6,9 +6,6 @@ import type { LayoutServerLoad } from './$types';
 /**
  * Enables SSR support by passing the initial auth state
  * to the return object of the load function.
- *
- * @param locals - An object containing the authentication information.
- * @returns An object merging Clerk props for server-side rendering.
  */
 export const load: LayoutServerLoad = ({ locals }) => {
 	return {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,16 @@
+<!--
+The <ClerkProvider> component wraps the entire application and provides 
+context for user authentication. It ensures that all child components 
+have access to Clerk's authentication state, session, and user data.
+
+This is required for using Clerk's prebuilt components, runes, and helpers 
+throughout the app. All components that rely on Clerk must be nested 
+within this provider.
+
+For more details, refer to the Svelte Clerk documentation:
+https://svelte-clerk.netlify.app/ssr/quickstart.html
+-->
+
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import { ClerkProvider } from 'svelte-clerk';
