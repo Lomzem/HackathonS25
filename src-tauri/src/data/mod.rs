d24896e2aa0af79675bad9da58299176d9ae9e61
riley-1995 <rileydenn@gmail.com>
2025-04-26 03:41:14 -0700

    Add affirmation to mod.rs

--- src-tauri/src/data/mod.rs
+++ src-tauri/src/data/mod.rs
@@ -3,9 +3,11 @@ use std::sync::LazyLock;
 use native_db::Models;
 
 pub mod exercise;
+pub mod affirmations;
 
 static DATABASE_MODELS: LazyLock<Models> = LazyLock::new(|| {
     let mut models = Models::new();
     models.define::<exercise::Exercise>().unwrap();
+    models.define::<affirmations::Affirmations>().unwrap();
     models
 });
